15280462338002882aa4aaa7d92b0d7433dbff97
Johnatan Lima <joh23nathan@gmail.com>
2019-11-04 23:26:33 -0200

    Criação de um component - Done

---
+++ myApp/src/app/MyComponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'MyComponent',
+    templateUrl: './MyComponent.component.html'        
+})
+
+export class MyComponent{
+    
+}
