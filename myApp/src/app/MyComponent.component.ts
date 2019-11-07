42375dc04c3f313a4ac8a53cf54a63c41b3fc8ea
Johnatan Lima <joh23nathan@gmail.com>
2019-11-06 23:31:58 -0200

    Estilização dos componentes

--- myApp/src/app/MyComponent.component.ts
+++ myApp/src/app/MyComponent.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'MyComponent',
-    templateUrl: './MyComponent.component.html'        
+    templateUrl: './MyComponent.component.html',
+    styleUrls: ['./MyComponent.component.css']       
 })
 
 export class MyComponent{
