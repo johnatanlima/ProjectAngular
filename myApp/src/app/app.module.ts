cef38d6202a361df23ae84141e48226b667b79e5
Johnatan Lima <joh23nathan@gmail.com>
2019-11-08 21:13:37 -0200

    Trabalhando com pacote Material

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyComponent } from './MyComponent.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { MyComponent } from './MyComponent.component';
     MyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
